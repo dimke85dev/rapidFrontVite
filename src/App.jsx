a1b01605078113e0a2a123a053c2418556d462a1
Dimke85dev <dimke85.dev@gmail.com>
2023-07-25 11:08:23 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/UI/Footer';
 
@@ -19,11 +19,11 @@ import Layout from './components/layout/Layout';
 // import AddCar from './components/addCar/AddCar';
 // import MainRepairsPage from './pages/MainRepairsPage';
 // import TypeRepairPage from './pages/TypeRepairPage';
-// import Price from './pages/Price';
+import Price from './pages/Price';
 // import Users from './pages/Users';
 // import Cars from './pages/Cars';
 // import Car from './pages/Car';
-// import Contacts from './pages/Contacts';
+import Contacts from './pages/Contacts';
 import { useDispatch } from 'react-redux';
 import { getMe } from './store/features/auth/authSlice';
 import { ToastContainer } from 'react-toastify';
@@ -45,37 +45,37 @@ function App() {
     <>
     <BrowserRouter>
           <Layout >
-          {/* <Routes>
-          <Route path="/" element={<MainPage />} />
+          <Routes>
+          {/* <Route path="/" element={<MainPage />} /> */}
 
 
-          <Route path="/posts" element={<PostsPage />} />
-          <Route path="/out" element={<MainPage />} />
-          <Route path="/:id" element={<PostPage />} />
-          <Route path="/newPost" element={<AddPostPage />} />
-          <Route path="/:id/editPost" element={<EditPostPage />} />
+          {/* <Route path="/posts" element={<PostsPage />} /> */}
+          {/* <Route path="/out" element={<MainPage />} /> */}
+          {/* <Route path="/:id" element={<PostPage />} /> */}
+          {/* <Route path="/newPost" element={<AddPostPage />} /> */}
+          {/* <Route path="/:id/editPost" element={<EditPostPage />} /> */}
 
-          <Route path="/register" element={<RegisterPage />} />
+          {/* <Route path="/register" element={<RegisterPage />} /> */}
           <Route path="/price" element={<Price />} />
 
-          <Route path="/about" element={<AboutPages />} />
+          {/* <Route path="/about" element={<AboutPages />} /> */}
           <Route path="/contacts" element={<Contacts />} />
-          <Route path="/cars" element={<Cars />} />
-          <Route path="/cars/:id" element={<Car />} />
+          {/* <Route path="/cars" element={<Cars />} /> */}
+          {/* <Route path="/cars/:id" element={<Car />} /> */}
 
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/users" element={<Users />} />
+          {/* <Route path="/login" element={<LoginPage />} /> */}
+          {/* <Route path="/users" element={<Users />} /> */}
 
-          <Route path="/takeacar" element={<TakeACar />} />
-          <Route path="/addcar/:id" element={<AddCar />} />
-          <Route path="/addcarrepair" element={<AddCarRepair />} />
+          {/* <Route path="/takeacar" element={<TakeACar />} /> */}
+          {/* <Route path="/addcar/:id" element={<AddCar />} /> */}
+          {/* <Route path="/addcarrepair" element={<AddCarRepair />} /> */}
 
-          <Route path="/mainrepair" element={<MainRepairsPage />} />
-          <Route path="/mainrepair/:id" element={<TypeRepairPage />} />
+          {/* <Route path="/mainrepair" element={<MainRepairsPage />} /> */}
+          {/* <Route path="/mainrepair/:id" element={<TypeRepairPage />} /> */}
 
-          <Route path="/reports" element={<Reports />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes> */}
+          {/* <Route path="/reports" element={<Reports />} /> */}
+          {/* <Route path="/settings" element={<Settings />} /> */}
+        </Routes>
 
             <ToastContainer position="bottom-right" />
 
